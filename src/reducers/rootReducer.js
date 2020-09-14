b5d3a6cf87a7e707cad9ba1a82d023fbcb475f07
OtsoLap <59653348+otsolap@users.noreply.github.com>
2020-09-14 10:39:03 +0300

    folder structure created and middleware installed to package.json log.

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,1 @@
+import { combinereducers} from 'redux';
---
+++ src/server.js
@@ -0,0 +1,4 @@
+// This is for my backend.
+// express, body-parser, axios/fetch, maybe firebase.
+// will test with postman or firebase. have not yet decided.
+// will replace dummydate with mongoose/mongodb localstorage later.
